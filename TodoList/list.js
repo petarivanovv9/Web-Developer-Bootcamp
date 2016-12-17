82291e0bab1975386cae90ff0d9df87f423ab3f7
Petar Ivanov <pepincho95@gmail.com>
2016-12-17 14:39:07 +0200

    Add JS TodoList exercise solution-v02.

--- TodoList/list.js
+++ TodoList/list.js
@@ -6,13 +6,35 @@ var input = prompt("What would you like to do?");
 
 while (input !== "quit") {
   if (input === "list") {
-    console.log(todos);
+    listTodos();
   }
   else if (input === "new") {
-    var newTodo = prompt("Enter new todo:");
-    todos.push(newTodo);
+    addTodo();
+  }
+  else if (input === "delete") {
+    deleteTodo();
   }
   input = prompt("What would you like to do?");
 }
 
 console.log("Ok, You quit the app!");
+
+function listTodos() {
+  console.log("**********");
+  todos.forEach(function(todo, idx){
+    console.log(idx + ": " + todo);
+  });
+  console.log("**********");
+}
+
+function addTodo() {
+  var newTodo = prompt("Enter new todo:");
+  todos.push(newTodo);
+  console.log("Added Todo");
+}
+
+function deleteTodo() {
+  var indexToDel = prompt("Enter index of todo to delete:");
+  todos.splice(indexToDel, 1);
+  console.log("Deleted Todo");
+}
