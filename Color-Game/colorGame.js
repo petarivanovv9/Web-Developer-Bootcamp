458e43e45dad5a4fb99be5126a758fd02f54321d
Petar Ivanov <pepincho95@gmail.com>
2016-12-22 22:14:00 +0200

    Add Color Game project, part 01, v01.

---
+++ Color-Game/colorGame.js
@@ -0,0 +1,30 @@
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll(".square");
+
+var pickedColor = colors[3];
+
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].style.background = colors[i];
+
+  squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.background;
+
+    if (clickedColor === pickedColor){
+      alert("Correct!");
+    } else {
+      alert("Wrong!");
+    }
+  });
+}
