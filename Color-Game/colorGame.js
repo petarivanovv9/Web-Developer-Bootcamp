334ce3c2c8f3579f6b7e68675f41ee509de663ac
Petar Ivanov <pepincho95@gmail.com>
2016-12-23 14:30:49 +0200

    Add Color Game project, part 04, v04.

--- Color-Game/colorGame.js
+++ Color-Game/colorGame.js
@@ -1,10 +1,22 @@
 var colors = generateRandomColors(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+  }
+
+  h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -16,6 +28,7 @@ for (var i = 0; i < squares.length; i++) {
 
     if (clickedColor === pickedColor){
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?";
       changeColors(clickedColor);
       h1.style.background = clickedColor;
     } else {
