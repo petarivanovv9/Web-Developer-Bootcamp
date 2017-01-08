92dcb5bb48c91518c1bee84f5287ee0c2b6fb238
Petar Ivanov <pepincho95@gmail.com>
2017-01-08 15:49:41 +0200

    Add adding new todos func to TodoList project.

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,10 +1,18 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("complited");
 })
 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 })
