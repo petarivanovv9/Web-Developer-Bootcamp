6b1fb05675c2450b9972fdeec6f6c9a79e09d1fe
Petar Ivanov <pepincho95@gmail.com>
2017-01-08 16:30:50 +0200

    Add background color, plus and trash icons with styling.

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -13,6 +13,6 @@ $("input[type='text']").keypress(function(event){
   if (event.which === 13) {
     var todoText = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</li>");
   }
 })
