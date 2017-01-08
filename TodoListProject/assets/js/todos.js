e0743aa28243237a91c669dcdaf9e22d8c4ff18f
Petar Ivanov <pepincho95@gmail.com>
2017-01-08 15:37:32 +0200

    Add remove todo func to TodoList project.

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,3 +1,10 @@
 $("li").click(function(){
   $(this).toggleClass("complited");
 })
+
+$("span").click(function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+})
