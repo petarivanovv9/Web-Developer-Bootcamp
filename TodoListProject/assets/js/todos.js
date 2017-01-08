54a2ea126b889f1b218cbc27fa1171ae4ca83e4b
Petar Ivanov <pepincho95@gmail.com>
2017-01-08 16:36:18 +0200

    Add hide/show input func and order css code.

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -16,3 +16,7 @@ $("input[type='text']").keypress(function(event){
     $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</li>");
   }
 })
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
+})
