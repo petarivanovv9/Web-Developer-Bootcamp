ddbec7d818f5478c4fd6c690ce85600153e34230
Petar Ivanov <pepincho95@gmail.com>
2017-02-27 17:10:47 +0200

    Add more get routes to FirstExpress app.

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -3,8 +3,20 @@ var app = express();
 
 
 app.get("/", function (req, res) {
+  console.log("Someone made a request to /!");
   res.send("Hi there!");
 });
+
+app.get("/bye", function (req, res) {
+  console.log("Someone made a request to /bye!");
+  res.send("Goodbye!");
+});
+
+app.get("/dog", function (req, res) {
+  console.log("Someone made a request to /dog!");
+  res.send("MEOW!");
+});
+
 
 
 app.listen(3000, function () {
