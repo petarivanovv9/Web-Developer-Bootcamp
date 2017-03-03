b81761ababf96a8fe4ebe3d31a988c6fbc4dc5b7
Petar Ivanov <pepincho95@gmail.com>
2017-03-03 16:58:10 +0200

    Add route params examples in FirstExpressApp.

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -17,6 +17,22 @@ app.get("/dog", function (req, res) {
   res.send("MEOW!");
 });
 
+// Route params example 1
+app.get("/r/:subredditName", function(req, res) {
+  var subreddit = req.params.subredditName;
+  // console.log(req.params["subredditName"]);
+  res.send("Welcome to the " + subreddit + " subreddit!");
+});
+
+// Route params example 2
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res) {
+  res.send("Welcome to the comments page!");
+});
+
+// `*` route mather - "catch all"
+app.get("*", function (req, res) {
+  res.send("You are a star!");
+});
 
 
 app.listen(3000, function () {
