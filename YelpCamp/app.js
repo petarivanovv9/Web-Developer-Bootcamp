9e376bbb312b271784902a6bbc307649284d2312
Petar Ivanov <pepincho95@gmail.com>
2017-03-11 18:37:29 +0200

    Add YelpCamp: creating Campgrounds.

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+
+var campgrounds = [
+  {name: "Salmon Creek", image: "http://www.istockphoto.com/resources/images/PhotoFTLP/img_82250973.jpg"},
+  {name: "Sky", image: "https://www.aviary.com/img/photo-landscape.jpg"},
+  {name: "Mountain", image: "https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-144748015-1-1500x814.jpg"}
+];
 
 
 app.get("/", function(req, res) {
@@ -9,13 +18,20 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-  var campgrounds = [
-    {name: "Salmon Creek", image: "http://www.istockphoto.com/resources/images/PhotoFTLP/img_82250973.jpg"},
-    {name: "Sky", image: "https://www.aviary.com/img/photo-landscape.jpg"},
-    {name: "Mountain", image: "https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-144748015-1-1500x814.jpg"}
-  ];
-
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res) {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new.ejs");
 });
 
 
