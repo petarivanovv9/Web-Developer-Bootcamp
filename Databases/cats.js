46ed02f914ff1d3e27303e30a881a633cfdc06f3
Petar Ivanov <pepincho95@gmail.com>
2017-03-12 20:46:59 +0200

    Add intro to mongoose: part 2

--- Databases/cats.js
+++ Databases/cats.js
@@ -9,3 +9,42 @@ var catSchema = new mongoose.Schema({
 
 var Cat = mongoose.model("Cat", catSchema);
 
+
+var george = new Cat({
+  name: "George",
+  age: 11,
+  temperament: "Grouchy"
+});
+
+george.save(function(err, cat) {
+  if(err) {
+    console.log("Something went wrong!");
+  } else {
+    console.log("We saved a cat to the db.");
+    console.log(cat);
+  }
+})
+
+
+Cat.create({
+  name: "Snow White",
+  age: 15,
+  temperament: "Bland"
+}, function(err, cat) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(cat);
+  }
+});
+
+
+Cat.find({}, function(err, cats) {
+  if(err) {
+    console.log("ERROR");
+    console.log(err);
+  } else {
+    console.log("All the cats...");
+    console.log(cats);
+  }
+})
