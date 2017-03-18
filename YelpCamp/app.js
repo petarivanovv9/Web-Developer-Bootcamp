c07037e391d5953a5ccb1f01404ac1e9d9daafd0
Petar Ivanov <pepincho95@gmail.com>
2017-03-18 17:57:21 +0200

    Add YelpCamp: campground show page part 2.

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -29,7 +29,7 @@ app.get("/campgrounds", function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render("campgrounds", {campgrounds:allCampgrounds});
+      res.render("index", {campgrounds:allCampgrounds});
     }
   });
 });
@@ -38,7 +38,8 @@ app.get("/campgrounds", function(req, res) {
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image};
+  var description = req.body.description;
+  var newCampground = {name: name, image: image, description: description};
   Campground.create(newCampground, function(err, newlyCreated) {
     if (err) {
       console.log(err);
@@ -53,9 +54,15 @@ app.get("/campgrounds/new", function(req, res) {
   res.render("new.ejs");
 });
 
-// SHOW - show info about one campground
+// SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res) {
-  res.send("/campgrounds/:id");
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", {campground: foundCampground});
+    }
+  });
 });
 
 
