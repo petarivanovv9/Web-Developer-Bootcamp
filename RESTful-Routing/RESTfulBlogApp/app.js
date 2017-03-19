5d6a488f15ae25e595bed39897e3df8f69e5caae
Petar Ivanov <pepincho95@gmail.com>
2017-03-19 18:41:34 +0200

    Add RESTful Blog App: NEW and CREATE.

--- RESTful-Routing/RESTfulBlogApp/app.js
+++ RESTful-Routing/RESTfulBlogApp/app.js
@@ -32,12 +32,30 @@ app.get("/", function(req, res) {
   res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res) {
   Blog.find({}, function(err, blogs) {
     if (err) {
       console.log(err);
     } else {
       res.render("index", {blogs: blogs});
+    }
+  });
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+  res.render("new.ejs");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res) {
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      console.log(err);
+      res.render("new");
+    } else {
+      res.redirect("/blogs");
     }
   });
 });
