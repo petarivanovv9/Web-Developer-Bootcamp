1dc998be4d13fd899782ff56836c42a5e61b5611
Petar Ivanov <pepincho95@gmail.com>
2017-03-20 18:36:13 +0200

    Add RESTful Blog App: EDIT AND UPDATE.

--- RESTful-Routing/RESTfulBlogApp/app.js
+++ RESTful-Routing/RESTfulBlogApp/app.js
@@ -1,13 +1,15 @@
 var bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    express    = require("express"),
-    app        = express();
+methodOverride = require("method-override"),
+mongoose       = require("mongoose"),
+express        = require("express"),
+app            = express();
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -67,6 +69,28 @@ app.get("/blogs/:id", function(req, res) {
       res.redirect("/blogs");
     } else {
       res.render("show", {blog: foundBlog});
+    }
+  });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
     }
   });
 });
