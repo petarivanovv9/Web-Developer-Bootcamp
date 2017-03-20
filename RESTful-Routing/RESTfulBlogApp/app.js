fa15cec8eee6184d5c7b89b4a1c5e9e1af612cd8
Petar Ivanov <pepincho95@gmail.com>
2017-03-20 18:46:49 +0200

    Add RESTful Blog App: DESTROY.

--- RESTful-Routing/RESTfulBlogApp/app.js
+++ RESTful-Routing/RESTfulBlogApp/app.js
@@ -95,6 +95,17 @@ app.put("/blogs/:id", function(req, res) {
   });
 });
 
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res) {
+  Blog.findByIdAndRemove(req.params.id, req.body.blog, function(err) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
 
 app.listen(3000, function() {
   console.log("Server is listening...");
