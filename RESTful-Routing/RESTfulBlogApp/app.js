685faf491248ea9e6f5a33bc5c76c086e21bca41
Petar Ivanov <pepincho95@gmail.com>
2017-03-20 18:05:35 +0200

    Add RESTful Blog App: SHOW.

--- RESTful-Routing/RESTfulBlogApp/app.js
+++ RESTful-Routing/RESTfulBlogApp/app.js
@@ -60,6 +60,17 @@ app.post("/blogs", function(req, res) {
   });
 });
 
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", {blog: foundBlog});
+    }
+  });
+});
+
 
 app.listen(3000, function() {
   console.log("Server is listening...");
