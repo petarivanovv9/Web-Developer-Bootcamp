acccc9a331ee98cdd32bac9dfcf0e33f9618c688
Petar Ivanov <pepincho95@gmail.com>
2017-03-22 11:45:23 +0200

    Add Module.exports exercise.

---
+++ Associations/models/post.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+
+var postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+module.exports = mongoose.model("Post", postSchema);
--- Associations/references.js
+++ Associations/references.js
@@ -2,54 +2,40 @@ var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/blog_demo_2");
 
+var Post = require("./models/post");
+var User = require("./models/user");
 
-var postSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-});
-var Post = mongoose.model("Post", postSchema);
 
-var userSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-  posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Post"
-    }
-  ]
-});
-var User = mongoose.model("User", userSchema);
 
 // User.create({
 //   email: "Asen@asenov.bg",
 //   name: "Asen Asenov"
 // });
 
-// Post.create({
-//   title: "Hoooo 3",
-//   content: "hohoohoohoho"
-// }, function(err, post) {
-//   User.findOne({email: "Asen@asenov.bg"}, function(err, foundUser) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       foundUser.posts.push(post);
-//       foundUser.save(function(err, data) {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           console.log(data);
-//         }
-//       });
-//     }
-//   });
-// });
-
-User.findOne({email: "Asen@asenov.bg"}).populate("posts").exec(function(err, user) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(user);
-  }
+Post.create({
+  title: "Hoooo 4",
+  content: "aaaaaaaaaaaaaaaaaaaaa"
+}, function(err, post) {
+  User.findOne({email: "Asen@asenov.bg"}, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      foundUser.posts.push(post);
+      foundUser.save(function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+        }
+      });
+    }
+  });
 });
+
+// User.findOne({email: "Asen@asenov.bg"}).populate("posts").exec(function(err, user) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(user);
+//   }
+// });
