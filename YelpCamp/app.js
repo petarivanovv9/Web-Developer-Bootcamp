054175b1241d3fe248ec2aee2ef2489248daeea4
Petar Ivanov <pepincho95@gmail.com>
2017-03-24 15:53:17 +0200

    Add YelpCamp: Creating Comments Pt.2

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,6 +3,7 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
     seedDB      = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
@@ -68,6 +69,25 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
       console.log(err);
     } else {
       res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, function(err, comment) {
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
     }
   });
 });
