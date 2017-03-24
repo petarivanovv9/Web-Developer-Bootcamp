bb8e8a6455e526edd86164a1e6b3e883a9438be9
Petar Ivanov <pepincho95@gmail.com>
2017-03-24 16:19:14 +0200

    Add YelpCamp: Styling Comments Pt.2

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -10,6 +10,7 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 seedDB();
 
