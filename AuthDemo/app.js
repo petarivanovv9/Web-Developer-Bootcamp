e5ee642c38e2526eddc11a09e224b0d6682c7d5d
Petar Ivanov <pepincho95@gmail.com>
2017-03-26 21:32:51 +0300

    Add Secret Page Code Along Pt.5

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -36,7 +36,7 @@ app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/secret", function(req, res) {
+app.get("/secret", isLoggedIn, function(req, res) {
   res.render("secret");
 });
 
@@ -66,6 +66,17 @@ app.post("/login", passport.authenticate("local", {
 }), function(req, res) {
 });
 
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(3000, function() {
   console.log("Server is listening...");
