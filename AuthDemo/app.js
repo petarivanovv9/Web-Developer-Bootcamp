4f102fe5bddadac126afdf1f6ce6c9ebfbd679c5
Petar Ivanov <pepincho95@gmail.com>
2017-03-26 21:03:47 +0300

    Add Secret Page Code Along Pt.2

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,14 +1,29 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+var express               = require("express"),
+    bodyParser            = require("body-parser"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User                  = require("./models/user");
 
 var app = express();
 
 mongoose.connect("mongodb://localhost/auth_demo");
 
+app.use(require("express-session")({
+  secret: "Petar is an awesome person",
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res) {
