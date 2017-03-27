6644d89c86bbe3d99701c264e4bbe25ef232f703
Petar Ivanov <pepincho95@gmail.com>
2017-03-27 13:47:26 +0300

    Add YelpCamp: Adding Auth Pt.2

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -17,6 +17,19 @@ app.use(express.static(__dirname + "/public"));
 
 seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "I am awesome!",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.get("/", function(req, res) {
   res.render("landing");
 });
@@ -93,6 +106,27 @@ app.post("/campgrounds/:id/comments", function(req, res) {
         }
       });
     }
+  });
+});
+
+// ============
+// AUTH ROUTES
+// ============
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function() {
+      res.redirect("/campgrounds");
+    });
   });
 });
 
