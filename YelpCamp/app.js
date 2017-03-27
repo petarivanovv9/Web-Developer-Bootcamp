88148928540ffcf5c8b36750a8290f6c4e10eacb
Petar Ivanov <pepincho95@gmail.com>
2017-03-27 13:52:58 +0300

    Add YelpCamp: Adding Auth Pt.3

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -130,6 +130,17 @@ app.post("/register", function(req, res) {
   });
 });
 
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), function(req, res) {
+});
+
 
 app.listen(3000, function() {
   console.log("Server is listening...");
