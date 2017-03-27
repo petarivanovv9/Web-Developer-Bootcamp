bf113a03bc2e3375d41b6052edd07ed785b09114
Petar Ivanov <pepincho95@gmail.com>
2017-03-27 13:36:56 +0300

    Add YelpCamp: Adding Auth Pt.1

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,10 +1,13 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    seedDB      = require("./seeds");
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Campground    = require("./models/campground"),
+    Comment       = require("./models/comment"),
+    User          = require("./models/user"),
+    seedDB        = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 
