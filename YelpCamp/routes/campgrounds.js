af0fc1240ca48b2d1d50799c68debdb16daef31d
Petar Ivanov <pepincho95@gmail.com>
2017-04-13 21:51:31 +0300

    Add Campground Destroy.

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -76,6 +76,17 @@ router.put("/:id", function(req, res) {
   });
 });
 
+// DESTROY Campground route
+router.delete("/:id", function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
