d3e44d0f7adf9f122abe6447cbe77f4860ef60fb
Petar Ivanov <pepincho95@gmail.com>
2017-11-03 13:04:47 +0200

    Add YelpCamp: Comment Edit and Update

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -37,6 +37,28 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
+// comment edit route
+router.get("/:comment_id/edit", function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, foundComment) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+    }
+  });
+});
+
+// comment update route
+router.put("/:comment_id", function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
