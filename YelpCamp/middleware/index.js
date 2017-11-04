8cf0992bde8b46c0756e7329c7a3114151094228
Petar Ivanov <pepincho95@gmail.com>
2017-11-04 15:59:03 +0200

    Add YelpCamp: Flash Messages - Helpful Errors.

--- YelpCamp/middleware/index.js
+++ YelpCamp/middleware/index.js
@@ -9,16 +9,19 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
   if (req.isAuthenticated()) {
     Campground.findById(req.params.id, function(err, foundCampground) {
       if (err) {
+        req.flash("error", "Campground not found!");
         res.redirect("back");
       } else {
         if (foundCampground.author.id.equals(req.user._id)) {
           next();
         } else {
+          req.flash("error", "You don't have permission to do that!");
           res.redirect("back");
         }     
       }
     });
   } else {
+    req.flash("error", "You need to be logged in to do that!");
     res.redirect("back");
   }
 };
@@ -27,16 +30,19 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
   if (req.isAuthenticated()) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
       if (err) {
+        req.flash("error", "Comment not found!");
         res.redirect("back");
       } else {
         if (foundComment.author.id.equals(req.user._id)) {
           next();
         } else {
+          req.flash("error", "You don't have permission to do that!");
           res.redirect("back");
         }     
       }
     });
   } else {
+    req.flash("error", "You need to be logged in to do that!");
     res.redirect("back");
   }
 };
@@ -45,7 +51,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  req.flash("error", "Please login first!");
+  req.flash("error", "You need to be logged in to do that!");
   res.redirect("/login");
 };
 
--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -19,18 +19,18 @@ router.get("/new", middleware.isLoggedIn, function(req, res) {
 router.post("/", middleware.isLoggedIn, function(req, res) {
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
-      console.log(err);
       res.redirect("/campgrounds");
     } else {
       Comment.create(req.body.comment, function(err, comment) {
         if (err) {
-          console.log(err);
+          req.flash("error", "Something went wrong!");
         } else {
           comment.author.id = req.user._id;
           comment.author.username = req.user.username;
           comment.save();
           campground.comments.push(comment);
           campground.save();
+          req.flash("success", "Successfully added comment!");
           res.redirect("/campgrounds/" + campground._id);
         }
       });
@@ -66,6 +66,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
     if (err) {
       res.redirect("back");
     } else {
+      req.flash("success", "Comment deleted!");
       res.redirect("/campgrounds/" + req.params.id);
     }
   });
